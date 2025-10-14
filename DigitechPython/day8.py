50503feae4e4351e8f7bae1364a78600f425b009
Maxwell6713 <maxwelljoel2004@gmail.com>
2025-10-14 12:17:28 +0300

    day8

---
+++ DigitechPython/day8.py
@@ -0,0 +1,27 @@
+# # defining a function
+# def get_pass(name, age):
+#     if age <= 17:
+#         return f"{name}, you are not allowed to enter the club."
+#     else:
+#         return f"{name}, you are allowed to enter the club."
+    
+#     # generate the actual name and age
+# user_name = input("Enter your name: ")
+# user_age = int(input("Enter your age: "))
+
+# final = get_pass(user_name, user_age)
+# print(final)
+
+
+
+
+
+def get_access():
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    if age <= 17:
+        print (f"{name}, you are not allowed to enter the club.")
+    else:
+        print (f"{name}, you are allowed to enter the club.")
+
+get_access() 
